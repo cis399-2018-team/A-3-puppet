03993d4cc3463647e3f115cf65823c4895888bc7
root <root@ip-10-0-2-32.us-west-2.compute.internal>
2018-07-13 00:14:31 +0000

    Commit new

---
+++ manifests/site.pp
@@ -0,0 +1,14 @@
+
+node ip-10-0-2-32 {
+	cron { "puppet update":
+	    command => "cd /etc/puppet && git pull -q origin master",
+	    user    => root,
+	    minute  => "*/5",
+	}
+}
+
+node ip-10-0-2-181 {
+}
+
+node ip-10-0-2-73 {
+}
---
+++ modules/ssh/manifests/init.pp
@@ -0,0 +1,33 @@
+ass sshd {
+	package {
+		"openssh-server": ensure => installed;
+	}
+
+	file { "/etc/ssh/sshd_config":
+		source  => [
+			# from modules/ssh/sshd_config
+			"puppet:///modules/ssh/sshd_config",
+		],
+		mode    => 444,
+		owner   => root,
+		group   => root,
+		# package must be installed before configuration file
+		require => Package["openssh-server"],
+	}
+
+	service { "sshd":
+		# automatically start at boot time
+		enable     => true,
+		# restart service if it is not running
+		ensure     => running,
+		# "service sshd status" returns useful service status info
+		hasstatus  => true,
+		# "service sshd restart" can restart service
+		hasrestart => true,
+		# package and configuration must be present for service
+		require    => [ Package["openssh-server"],
+			        File["/etc/ssh/sshd_config"] ],
+		# changes to configuration cause service restart
+		subscribe  => File["/etc/ssh/sshd_config"],
+	}
+}
